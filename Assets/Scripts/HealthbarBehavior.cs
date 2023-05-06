58769633c577f244dae7b5a63cdf55ae0f52ea67
nguyen viet <20522152@gm.uit.edu.vn>
2023-05-06 18:17:56 +0700

    feat : fix healbar canvas error

--- Assets/Scripts/HealthbarBehavior.cs
+++ Assets/Scripts/HealthbarBehavior.cs
@@ -1,27 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
-using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthbarBehavior : MonoBehaviour
 {
 
-    [SerializeField] private Slider slider;
+    [SerializeField] private GameObject slider;
 
-    [SerializeField] private Color low;
+    //[SerializeField] private Color low;
 
-    [SerializeField] private Color high;
+    //[SerializeField] private Color high;
 
-    [SerializeField] private Vector3 offSet;
+    //[SerializeField] private Vector3 offSet;
 
+    [SerializeField] private Image healthBar;
+    [SerializeField] private Image healthAni;
 
     public void SetHealth(float health, float maxHealth)
     {
-        slider.gameObject.SetActive(health < maxHealth);
-        slider.value = health;
-        slider.maxValue = maxHealth;
+        slider.SetActive(health < maxHealth);
+        //slider.value = health;
+        //slider.maxValue = maxHealth;
+        healthBar.fillAmount = (float)health / maxHealth;  
+        //slider.fillRect.GetComponentInChildren<Image>().color = Color.Lerp(low, high, slider.normalizedValue);
+    }
 
-        slider.fillRect.GetComponentInChildren<Image>().color = Color.Lerp(low, high, slider.normalizedValue);
+    private void FixedUpdate()
+    {
+        healthAni.fillAmount = Mathf.Lerp(healthAni.fillAmount, healthBar.fillAmount, Time.fixedDeltaTime );
     }
 }
---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+    }
+}
